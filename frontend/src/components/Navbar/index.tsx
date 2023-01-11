31ccc99a7bc0d86b7a01072d17c3ec686d62aaf8
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-01-10 21:22:42 -0300

    feat: navbar component

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -3,9 +3,22 @@ import './styles.css';
 const Navbar = () => {
 
     return (
-        <div className="navbar-container">
-            <h1> Navbar </h1>
-        </div>
+        <nav className="navbar navbar-expand-md navbar-dark bg-primary navbar-container">
+            <div className="container-fluid">
+                <h4>AdoPet</h4>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#adopet-navbar"
+                    aria-controls="adopet-navbar"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
     )
 }
 

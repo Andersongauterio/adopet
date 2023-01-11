7d2fc9f57f677844c622092b344345f469172022
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-01-10 21:22:23 -0300

    feat: footer component

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer navbar-fixed-bottom footer-container text-center bg-primary">
+            <h6>Anderson Gautério</h6>
+        </footer>
+    );
+};
+
+export default Footer;

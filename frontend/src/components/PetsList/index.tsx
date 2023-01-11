663a9022943050827cd8278591e3b6b1dc5cd0b3
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-01-10 21:23:25 -0300

    feat: petList component

---
+++ frontend/src/components/PetsList/index.tsx
@@ -0,0 +1,71 @@
+import PetCard from '../PetCard';
+import './styles.css';
+
+const PetsList = () => {
+
+    return (
+        <div className="pets-list-container">
+            <div className="row">
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+                <div className="col-sm-6 col-lg-4 col-xl-2">
+                    <PetCard />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default PetsList;

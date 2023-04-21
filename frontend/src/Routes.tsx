e641ffc0de531eb24ecf1bf3c3fc383ab3b58370
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-04-20 21:59:00 -0300

    chore: adjust routes

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,18 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PetDetails from './components/PetDetails';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import PetDetails from "./components/PetDetails";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
 
 const AppRoutes = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/pet" element={<PetDetails />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/pet" element={<PetDetails />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    );
 };
 
 export default AppRoutes;

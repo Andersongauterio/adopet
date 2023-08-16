2638fa46913be0e6dd1b48776ff0345aca9492a1
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-08-15 21:32:46 -0300

    pet dto and post initial

---
+++ backend/src/pets/dtos/createPet.dto.ts
@@ -0,0 +1,9 @@
+export interface CreatePetDTO {
+  id: number;
+  name: string;
+  description: string;
+  birth_date: string;
+  type: string;
+  size: string;
+  include_date: string;
+}
--- backend/src/pets/pets.controller.ts
+++ backend/src/pets/pets.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { CreatePetDTO } from './dtos/createPet.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('pets')
 export class PetsController {
   @Get()
   async getAllPets() {
     return JSON.stringify({ pet1: 'teste' });
+  }
+
+  @Post()
+  async createPet(@Body() createPet: CreatePetDTO) {
+    return createPet;
   }
 }

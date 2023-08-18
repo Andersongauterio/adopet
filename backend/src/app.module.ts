c1bbe89abb9d79e560f64cc84cc8e270f742ff8a
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-08-17 22:00:13 -0300

    config postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,22 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { PetsModule } from './pets/pets.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PetsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.development.local',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+    }),
+    PetsModule,
+  ],
 })
 export class AppModule {}

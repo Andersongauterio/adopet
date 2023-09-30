94451940dc660ce7a19a55db144d2748fbf262d5
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-09-29 23:00:32 -0300

    feat: adjust footer layout

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,13 +1,27 @@
+import { Link } from 'react-router-dom';
+import { ReactComponent as GithubIcon } from '../../assets/images/github.svg';
+import { ReactComponent as LinkedinIcon } from '../../assets/images/linkedin.svg';
 import "./styles.css";
 
 const Footer = () => {
-    return (
-        <footer className="bg-primary text-white py-3 mt-auto">
-            <div className="container text-center">
-                <p>Footer content goes here</p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="bg-primary text-white py-3 mt-auto adopet-footer-container">
+      <div className="adopet-footer-text">
+        <Link to="https://github.com/AndersonGauterio">
+          <div className="adopet-footer-icon">
+            <GithubIcon />
+            <h6>Anderson Gautério</h6>
+          </div>
+        </Link>
+        <Link to="https://br.linkedin.com/in/anderson-gautério-a57203142">
+          <div className="adopet-footer-icon">
+            <LinkedinIcon />
+            <h6>Anderson Gautério</h6>
+          </div>
+        </Link>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

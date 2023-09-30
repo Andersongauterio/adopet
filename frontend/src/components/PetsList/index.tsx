1fc8bd059abe4a3ec10dab2591ceafca8acea92d
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-09-30 10:14:59 -0300

    refactor: Pet list refactor

--- frontend/src/components/PetsList/index.tsx
+++ frontend/src/components/PetsList/index.tsx
@@ -14,7 +14,7 @@ const PetsList = () => {
       telefone: 51984322799,
       description: "Pet muito querido buscando um lar",
       size: "G",
-      imgUrl: "https://vetsmart-parsefiles.s3.amazonaws.com/3327f2ceb6e24aad97bdb7dfe824fd9c_breed.jpg",
+      imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/YellowLabradorLooking_new.jpg/200px-YellowLabradorLooking_new.jpg",
       cidade: "Dois Irmãos/RS"
     },
     {
@@ -32,51 +32,6 @@ const PetsList = () => {
       telefone: 51984322799,
       description: "Pet muito querido buscando um lar",
       size: "G",
-      imgUrl: "https://vetsmart-parsefiles.s3.amazonaws.com/3327f2ceb6e24aad97bdb7dfe824fd9c_breed.jpg",
-      cidade: "Dois Irmãos/RS"
-    },
-    {
-      id: 4,
-      name: "Penny",
-      telefone: 51984322799,
-      description: "Pet muito querido buscando um lar",
-      size: "G",
-      imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/YellowLabradorLooking_new.jpg/200px-YellowLabradorLooking_new.jpg",
-      cidade: "Dois Irmãos/RS"
-    },
-    {
-      id: 5,
-      name: "Marley",
-      telefone: 51984322799,
-      description: "Pet muito querido buscando um lar",
-      size: "G",
-      imgUrl: "https://vetsmart-parsefiles.s3.amazonaws.com/3327f2ceb6e24aad97bdb7dfe824fd9c_breed.jpg",
-      cidade: "Dois Irmãos/RS"
-    },
-    {
-      id: 6,
-      name: "Penny",
-      telefone: 51984322799,
-      description: "Pet muito querido buscando um lar",
-      size: "G",
-      imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/YellowLabradorLooking_new.jpg/200px-YellowLabradorLooking_new.jpg",
-      cidade: "Dois Irmãos/RS"
-    },
-    {
-      id: 7,
-      name: "Marley",
-      telefone: 51984322799,
-      description: "Pet muito querido buscando um lar",
-      size: "G",
-      imgUrl: "https://vetsmart-parsefiles.s3.amazonaws.com/3327f2ceb6e24aad97bdb7dfe824fd9c_breed.jpg",
-      cidade: "Dois Irmãos/RS"
-    },
-    {
-      id: 8,
-      name: "Penny",
-      telefone: 51984322799,
-      description: "Pet muito querido buscando um lar",
-      size: "G",
       imgUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/YellowLabradorLooking_new.jpg/200px-YellowLabradorLooking_new.jpg",
       cidade: "Dois Irmãos/RS"
     }
@@ -88,45 +43,14 @@ const PetsList = () => {
     setSelectedPet(pet);
   };
 
-  const closeModal = () => {
-    setSelectedPet(null);
-  };
-
   return (
     <div className="pets-list-container">
       <div className="row">
         {pets.map(pet => (
-          <div className="col-sm-12s col-lg-6 col-xl-3" key={pet.id}>
+          <div className="col-sm-12s col-lg-5 col-xl-4" key={pet.id}>
             <PetCard pet={pet} onClick={() => handlePetCardClick(pet)} />
           </div>
         ))}
-        {selectedPet && (
-        <Modal 
-          isOpen={true} 
-          onRequestClose={closeModal} style={{
-          overlay: {
-            backgroundColor: 'rgba(0, 0, 0, 0.5)'
-          },
-          content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            width: '30%',
-            maxHeight: '80%',
-            overflow: 'auto'
-          }
-        }}>
-          <div className="pet-card-modal-container">
-            <PetCardDetails pet={selectedPet} />
-            <button 
-              className="btn btn-primary"
-              onClick={closeModal}>Fechar</button>
-          </div>
-        </Modal>
-      )}
       </div>
     </div>
   );

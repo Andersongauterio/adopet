6184ddd0c7954d0c841500c2bec241384a232b9f
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-10-02 21:54:45 -0300

    feat: filter component style

--- frontend/src/components/PetFilter/index.tsx
+++ frontend/src/components/PetFilter/index.tsx
@@ -1,12 +1,60 @@
 import "./styles.css";
+import Select from 'react-select';
+
+const especies = [
+  { label: 'Cachorro', value: 'cachorro' },
+  { label: 'Gato', value: 'gato' },
+  { label: 'Hamster', value: 'hamster' },
+];
+
+const generos = [
+  { label: 'Macho', value: 'macho' },
+  { label: 'Fêmea', value: 'femea' },
+];
+
+const handleChangeEspecie = () => {
+  console.log('Change especie')
+};
+
+const handleChangeGenero = () => {
+  console.log('Change especie')
+}
+
 
 const PetFilter = () => {
 
-    return (
-        <div className="pet-filter-container">
-            <h1>Pet Filter</h1>
+  return (
+    <div className="adopet-filter-container">
+      <div className="adopet-filter-header">
+        <h3>Filtros</h3>
+      </div>
+      <div className="adopet-filter-filters">
+        <div className="adopet-select-filter-control">
+          <Select
+            options={especies}
+            isClearable
+            placeholder="Espécies"
+            onChange={handleChangeEspecie}
+            isMulti={false}
+            classNamePrefix="adopet-filter-select"
+          />
         </div>
-    );
+        <div className="adopet-select-filter-control">
+          <Select
+            options={generos}
+            isClearable
+            placeholder="Gênero"
+            onChange={handleChangeGenero}
+            isMulti={false}
+            classNamePrefix="adopet-filter-select"
+          />
+        </div>
+      </div>
+      <div className="adopet-filter-button">
+        <button className="btn btn-secondary btn-adopet-filter">Filtrar</button>
+      </div>
+    </div>
+  );
 };
 
 export default PetFilter;

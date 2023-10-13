ab4564521f65301857265e45fa7518683994f649
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-10-13 18:31:48 -0300

    feat: adoption page

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import Adote from "./pages/Adote";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import PetCadastro from "./pages/PetCadastro";
@@ -19,6 +20,7 @@ const AppRoutes = () => {
         <Route path="/petCardDetails/:petId" element={<PetDetail />} />
         <Route path="/userArea/:userId" element={<UserArea />} />
         <Route path="/petCadastro" element={<PetCadastro />} />
+        <Route path="/adotar/:petId" element={<Adote />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ frontend/src/pages/Adote/index.tsx
@@ -0,0 +1,13 @@
+import FormAdocao from '../../components/FormAdocao';
+import './styles.css';
+
+const Adote = () => {
+  return (
+    <div className='adopet-adote-container'>
+      <FormAdocao />
+    </div>
+  );
+};
+
+export default Adote;
+

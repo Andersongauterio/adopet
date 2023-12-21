818e30870d26b59927d1f249350a631b54d4c428
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-12-20 22:07:03 -0300

    feat: cad pet validation

--- frontend/src/components/FormPetCadastro/index.tsx
+++ frontend/src/components/FormPetCadastro/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Select from 'react-select';
+import { useForm, Controller } from 'react-hook-form';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './styles.css';
@@ -62,13 +63,6 @@ const FormPetCadastro = () => {
     }));
   };
 
-  const handleChangeType = (selectedOption: any) => {
-    setFormData(prevFormData => ({
-      ...prevFormData,
-      type: selectedOption ? selectedOption.value : null,
-    }));
-  };
-
   const handleChangeTamanho = (selectedOption: any) => {
     setFormData(prevFormData => ({
       ...prevFormData,
@@ -83,8 +77,9 @@ const FormPetCadastro = () => {
     }));
   };
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const { register, handleSubmit, control, formState: { errors } } = useForm<FormData>();
+
+  const onSubmit = async (data: FormData) => {
 
     const submittedData = {
       ...formData,
@@ -116,82 +111,116 @@ const FormPetCadastro = () => {
 
   return (
     <div className='adopet-form-pet-cadastro-container'>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className='adopet-form-pet-cadastro-fields'>
           <div className='adopet-form-pet-cadastro-part-1'>
             <div className='adopet-form-pet-cadastro-field'>
-              <input name="name"
+              <input
                 className='adopet-form-pet-cadastro-input-name'
-                value={formData.name}
-                onChange={handleChange}
-                id="name"
-                type="text"
+                type='text'
+                {...register('name', { required: 'Nome é obrigatório' })}
                 placeholder='Nome:' />
+              {errors.name && <p>{errors.name.message}</p>}
             </div>
             <div className='adopet-form-pet-cadastro-field'>
-              <Select
-                options={cidades}
-                isClearable
-                placeholder="Cidade"
-                onChange={handleChangeCidade}
-                isMulti={false}
-                classNamePrefix="adopet-form-pet-cadastro-select"
-                value={selectedCity}
+              <Controller
+                name="city_id"
+                control={control}
+                rules={{ required: 'Cidade é obrigatória' }}
+                render={({ field }) => (
+                  <Select
+                    {...field}
+                    options={cidades}
+                    classNamePrefix="adopet-form-pet-cadastro-select"
+                    placeholder="Cidade"
+                    value={cidades.find(option => option.value === (field.value ? field.value.toString() : ''))}
+                    onChange={val => field.onChange(val ? parseInt(val.value, 10) : null)}
+                  />
+                )}
               />
+              {errors.city_id && <p>{errors.city_id.message}</p>}
             </div>
             <div className='adopet-form-pet-cadastro-field'>
-              <Select
-                options={especies}
-                isClearable
-                placeholder="Espécie"
-                onChange={handleChangeType}
-                isMulti={false}
-                classNamePrefix="adopet-form-pet-cadastro-select"
-                value={selectedSpecies}
+              <Controller
+                name="species"
+                control={control}
+                rules={{ required: 'Espécie é obrigatória' }}
+                render={({ field }) => (
+                  <Select
+                    {...field}
+                    options={especies}
+                    classNamePrefix="adopet-form-pet-cadastro-select"
+                    placeholder="Espécie"
+                    value={especies.find(option => option.value === field.value)}
+                    onChange={val => field.onChange(val ? val.value : '')}
+                  />
+                )}
               />
+              {errors.species && <p>{errors.species.message}</p>}
             </div>
             <div className='adopet-form-pet-cadastro-field'>
-              <textarea rows={4}
-                name="description"
-                id="description"
-                placeholder="Descrição"
-                onChange={handleChange}
-                value={formData.description} />
+              <textarea
+                className='form-control'
+                rows={4}
+                {...register('description', {
+                  required: 'Descrição é obrigatória',
+                  minLength: { value: 10, message: "Descrição muito curta" }
+                })}
+                placeholder='Descrição'
+              />
+              {errors.description && <p>{errors.description.message}</p>}
             </div>
           </div>
           <div className='adopet-form-pet-cadastro-part-2'>
             <div className='adopet-form-pet-cadastro-field'>
-              <Select
-                options={generos}
-                isClearable
-                placeholder="Gênero"
-                onChange={handleChangeGender}
-                isMulti={false}
-                classNamePrefix="adopet-form-pet-cadastro-select"
-                value={selectedGender}
+              <Controller
+                name="gender"
+                control={control}
+                rules={{ required: 'Gênero é obrigatório' }}
+                render={({ field }) => (
+                  <Select
+                    {...field}
+                    options={generos}
+                    classNamePrefix="adopet-form-pet-cadastro-select"
+                    placeholder="Gênero"
+                    value={generos.find(option => option.value === field.value)}
+                    onChange={val => field.onChange(val ? val.value : '')}
+                  />
+                )}
               />
+              {errors.gender && <p>{errors.gender.message}</p>}
             </div>
             <div className='adopet-form-pet-cadastro-field'>
-              <Select
-                options={tamanhos}
-                isClearable
-                placeholder="Tamanho"
-                onChange={handleChangeTamanho}
-                isMulti={false}
-                classNamePrefix="adopet-form-pet-cadastro-select"
-                value={selectedSize}
+              <Controller
+                name="size"
+                control={control}
+                rules={{ required: 'Tamanho é obrigatório' }}
+                render={({ field }) => (
+                  <Select
+                    {...field}
+                    options={tamanhos}
+                    classNamePrefix="adopet-form-pet-cadastro-select"
+                    placeholder="Tamanho"
+                    value={tamanhos.find(option => option.value === field.value)}
+                    onChange={val => field.onChange(val ? val.value : '')}
+                  />
+                )}
               />
+              {errors.size && <p>{errors.size.message}</p>}
             </div>
             <div className='adopet-form-pet-cadastro-field'>
               <input
                 className='adopet-form-pet-cadastro-input-age'
-                value={formData.age}
-                onChange={handleChange}
-                name="age"
-                id="age"
-                type="number"
-                min="0"
-                placeholder='Idade:' />
+                type='number'
+                min='0'
+                {...register('age', {
+                  required: 'Idade é obrigatória',
+                  valueAsNumber: true,
+                  min: { value: 0, message: "Idade não pode ser negativa" }
+                })}
+                placeholder='Idade:'
+              />
+              {errors.age && <p>{errors.age.message}</p>}
               <label className='adopet-form-pet-cadastro-label-age'>Anos </label>
             </div>
             <div className='adopet-form-pet-cadastro-field'>
@@ -200,7 +229,7 @@ const FormPetCadastro = () => {
           </div>
         </div>
       </form>
-      <ToastContainer position="top-center" autoClose={3000} />
+      <ToastContainer position='top-center' autoClose={3000} />
     </div>
   )
 }

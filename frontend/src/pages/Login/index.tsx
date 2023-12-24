6d643e5cf0ab30ff843dc570525306a8fb622637
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-12-23 23:08:25 -0300

    feat: redirect to home is loged

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,7 +1,20 @@
 import FormLogin from '../../components/FormLogin';
 import './styles.css';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
+import { useEffect } from 'react';
 
 const Login = () => {
+
+  const navigate = useNavigate();
+  const { isLoggedIn } = useAuth();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate('/');
+    }
+  }, [isLoggedIn, navigate]);
+
   return (
     <div className='adopet-login-container'>
       <FormLogin />

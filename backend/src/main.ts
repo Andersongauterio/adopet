daf52cd1cb4c517b88d345ac91e36683606c0dc3
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-12-24 15:22:12 -0300

    fix: adjust Port for Railway

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ import { NestFactory } from '@nestjs/core';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(8080);
+  await app.listen(process.env.PORT || 8080);
 }
 
 bootstrap();

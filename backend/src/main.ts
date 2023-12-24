8acb8b680ee9d869792da3a5b65047c01e212e79
Andersongauterio <anderson.camara.gauterio@gmail.com>
2023-12-24 18:22:48 -0300

    fix: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,9 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://zingy-alfajores-b5880c.netlify.app',
+  });
   await app.listen(process.env.PORT || 8080);
 }
 
